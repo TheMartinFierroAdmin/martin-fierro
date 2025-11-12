50382d98b9aac0a042ba4321779659adfb091e2d
Franco <fmarchinitello@escuelasproa.edu.ar>
2025-11-12 18:44:47 -0300

    por favor funciona

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+nombre = "Martin Fierro"
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/menu2.html')
+def menu2():
+    return render_template('menu2.html')
+
+@app.route('/reseñas.html')
+def resenas():
+    return render_template('reseñas.html')
+
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/usuarios.html')
+def usuarios():
+    return render_template('usuarios.html')
+
+@app.route('/iniciosesion.html')
+def iniciosesion():
+    return render_template('iniciosesion.html')
+
+@app.route('/mis_reseñas.html')
+def mis_reseñas():
+    return render_template('mis_reseñas.html')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
